1e22f26048ac7b1abd1068dc99935d45f3267294
Pedro Pinas <joaopedrolp2204@gmail.com>
2025-03-27 15:30:59 -0300

    Start module restructuring

--- src/modules/scripts.js
+++ src/modules/scripts.js
@@ -1,12 +1,8 @@
 import dayjs from "dayjs";
-import { openingHours } from "./utils/opening-hours";
+import { openingHours } from "../utils/opening-hours";
 import { registerSchedule } from "../services/register-schedule";
 import { apiConfig } from "../services/api-config";
 
-/////////////////////////////////////////////////////////
-// CONSTANTES RELACIONADAS A CRIAÇÃO DE ELEMENTOS HTML //
-// CONSTANTES RELATIVAS A CRIAÇÃO DE ELEMENTOS HTML //
-/////////////////////////////////////////////////////////
 
 const today = dayjs(new Date()).format("YYYY-MM-DD");
 
@@ -26,22 +22,16 @@ const serviceDescriptionInput = document.getElementById("description");
 const dateInput = document.getElementById("date");
 const timeSelect = document.getElementById("hours");
 
-const selectHour = document.getElementById("hours"); //CREATE ELEMENT OPTION DENTRO DE hoursLoad()
-
+const selectHour = document.getElementById("hours");
 // Elementos da página principal para renderizar os agendamentos
 const dateInputMain = document.querySelector('.form-date-select .input input[type="date"].date');
 const periodMorningList = document.getElementById('period-morning');
 const periodAfternoonList = document.getElementById('period-afternoon');
 const periodNightList = document.getElementById('period-night');
 
-//Variavel que recebe o valor do date input, vai trocar toda hora
+//Variavel que recebe o valor do date input
 let selectedDate = dateInput.value;
 
-//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\||
-// !! Criação das funções abaixo !!  ||
-//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\||
-
-//Define a data no input da primeira página como a data atual do usuario
 export function todayDate() {
     dateInputMain.value = dayjs(new Date()).format("YYYY-MM-DD");
     console.log(
@@ -53,7 +43,6 @@ export function todayDate() {
     return dateInputMain;
 }
 
-//Abre o dialog ao clicar no botão de novo agendamento
 dialog.addEventListener("toggle", () => {
     const hideButton = dialogButton.style.display;
 
@@ -80,16 +69,11 @@ dateInput.addEventListener("change", () => {
     
 })
 
-//Faz ALGO ao enviar o SUBMIT do DIALOG ABERTO
 form.addEventListener("submit", async (event) => {
     event.preventDefault();
 
-    // **PRESERVA O VALOR DO dateInput ANTES DE LIMPAR OS CAMPOS**
-    const selectedDateValue = dateInput.value;
+    const selectedDateValue = dateInput.value
 
-
-    //Objeto que armazena os valores inseridos no input ANTES DO SUBMITE
-    //Valores são registrados no objeto NO MOMENTO em que o evento de SUBMIT é disparado
     const formData = {
         client: tutorNameInput.value,
         pet: petNameInput.value,
@@ -97,116 +81,90 @@ form.addEventListener("submit", async (event) => {
         description: serviceDescriptionInput.value,
         date: selectedDateValue, // Usa o valor preservado
         hour: timeSelect.value,
-    };
+    }
 
-    //LIMPA OS VALORES NOS INPUTS ASSIM QUE O USUARIO REALIZA O EVENTO DE SUBMIT
-    //TRANSFORMAR ISSO EM MÉTODO POSTERIORMENTE
     for (const key in formData) {
         if (formData.hasOwnProperty(key)) {
             const inputElement = document.getElementById(key);
             if (inputElement && key !== 'date') { // Não limpa o campo de data
-                inputElement.value = "";
+                inputElement.value = ""
             }
         }
     }
 
-    
-    //ENVIA OS DADOS PARA O SERVER.JSON
-    await registerSchedule(formData);
+    await registerSchedule(formData)
     renderSchedules(dateInputMain.value)
-    hoursLoad(selectedDateValue); // Refresh available hours after submission  // Usa o valor preservado
-    dialogDate(selectedDateValue) // Usa o valor preservado
-});
+    hoursLoad(selectedDateValue)
+    dialogDate(selectedDateValue) 
 
-//Define a data do dialog como data atual do usuario
+})
+
 export function dialogDate(date) {
-    //Cria data do dia atual
-
-    //Define data atual como valor do input de data
     dateInput.value = date
     console.log("Data do dialog", dateInput.value, dateInput);
 
-    //Não permite seleção de datas passadas
     dateInput.setAttribute("min", today);
 }
 
-//Define e cria os horários disponives para agendamento
 export async function hoursLoad(dataSelected) {
     try {
-        //Limpa quaisquer horários anteriores dentro do select
-        selectHour.innerHTML = "";
+        selectHour.innerHTML = ""
 
-        //Recebe valores dentro de schedules
-        const response = await fetch(`${apiConfig.baseURL}/schedules`);
-        const schedules = await response.json();
+        const response = await fetch(`${apiConfig.baseURL}/schedules`)
+        const schedules = await response.json()
 
-        console.log("Agendamentos carregados para o servidor", schedules);
+        console.log("Agendamentos carregados para o servidor", schedules)
 
-        //Filtra horários ocupados para a data selecionada
+
         const occupiedHours = schedules
             .filter((schedule) => schedule.date === dataSelected)
-            .map((schedule) => schedule.hour);
+            .map((schedule) => schedule.hour)
 
-        console.log("Horários ocupados para", dataSelected, ":", occupiedHours);
+        console.log("Horários ocupados para", dataSelected, ":", occupiedHours)
 
-        //Realiza uma função para cada item dentro do array openingHours
         openingHours.forEach((hour) => {
-            //Verifica se o horário já está ocupado
-            const isOccupied = occupiedHours.includes(hour);
-
-            // If the selected date is today, apply the unavailableHours filter
+            const isOccupied = occupiedHours.includes(hour)
             if (dayjs(dataSelected).isSame(dayjs(), 'day')) {
-                const now = dayjs();
-                const currentHour = now.format("HH:mm");
-                if (!isOccupied && hour > currentHour) { // Only add if it's not occupied and is later than the current time
-                    const options = document.createElement("option");
-                    options.value = hour;
-                    options.textContent = hour;
-                    selectHour.appendChild(options);
+                const now = dayjs()
+                const currentHour = now.format("HH:mm")
+                if (!isOccupied && hour > currentHour) { 
+                    const options = document.createElement("option")
+                    options.value = hour
+                    options.textContent = hour
+                    selectHour.appendChild(options)
                 }
             } else {
-                if (!isOccupied) { // If the selected date is not today, just filter by occupied hours
-                    const options = document.createElement("option");
-                    options.value = hour;
-                    options.textContent = hour;
-                    selectHour.appendChild(options);
+                if (!isOccupied) { 
+                    const options = document.createElement("option")
+                    options.value = hour
+                    options.textContent = hour
+                    selectHour.appendChild(options)
                 }
             }
         });
 
         if (selectHour.options.length === 0) {
-            const options = document.createElement("option");
-            options.value = "";
-            options.textContent = "Nenhum horário disponível";
-            options.disabled = true;
-            selectHour.appendChild(options);
+            const options = document.createElement("option")
+            options.value = ""
+            options.textContent = "Nenhum horário disponível"
+            options.disabled = true
+            selectHour.appendChild(options)
         }
     } catch (error) {
-        console.log(
-            "-------------Erro em hoursLoad()-----------\n",
-            error,
-            "\n----------------------------------------------"
-        );
+        console.log(error)
     }
 }
 
-//Função que bloqueia horários abaixo do horário atual
 export function unavailableHours() {
-    //Obtém DATA(Ano, mês, dia) e HORA atual
+    
     const now = dayjs();
-
-    //Obtem HORA atual
-    const hour = now.format("HH:mm"); //Hora atual
-
-    //Obtém DATA atual
-    const today = dayjs().format("YYYY-MM-DD"); //Data atual
-
-    //Guarda um array dentro de options | O array contem todos os option filhos de timeSelect
+    const hour = now.format("HH:mm"); 
+    const today = dayjs().format("YYYY-MM-DD"); 
     const options = timeSelect.querySelectorAll("option");
 
     if (dateInput.value == today) {
         options.forEach((option) => {
-            //Caso a option (horário) seja menor do que o horário do usuario, ele será removido do html
+
             if (option.value < hour) {
                 option.remove();
             }
@@ -217,7 +175,6 @@ export function unavailableHours() {
     console.log("Valor de date.input: ", dateInput.value);
 }
 
-// Função para criar um elemento de agendamento
 function createScheduleItem(schedule) {
     const listItem = document.createElement('li');
     listItem.dataset.id = schedule.id
@@ -252,7 +209,7 @@ function createScheduleItem(schedule) {
     removeButtonSpan.textContent = 'Remover agendamento';
 
     removeButtonSpan.addEventListener('click', async (event) => {
-        // Implementar a lógica de remoção aqui (usando o ID do agendamento, por exemplo)
+
         const item = event.target.closest("li")
         const id = item.dataset.id
 
@@ -268,21 +225,18 @@ function createScheduleItem(schedule) {
     return listItem;
 }
 
-
-//Renderiza agendamentos na pagina
 async function renderSchedules(date) {
-    // Limpa as listas existentes
+
     periodMorningList.innerHTML = '';
     periodAfternoonList.innerHTML = '';
     periodNightList.innerHTML = '';
 
     try {
         const response = await fetch(`${apiConfig.baseURL}/schedules`);
-        const data = await response.json(); // A resposta da API deve ser um objeto JSON
+        const data = await response.json(); 
 
-        // Verifique se 'data' é um array antes de prosseguir
         if (Array.isArray(data)) {
-            const schedules = data; // Agora 'schedules' é um array
+            const schedules = data; 
 
             const filteredSchedules = schedules.filter(schedule => schedule.date === date);
 
@@ -341,6 +295,8 @@ export async function removeSchedule(id) {
         alert("NãO FOI POSSIVEL CANCELAR")
     }
 }
+
+
 phoneInput.addEventListener("input", (event) => {
     let phoneValue = event.target.value;
 
@@ -360,7 +316,7 @@ phoneInput.addEventListener("input", (event) => {
     event.target.value = phoneValue;
 });
 
-//Reseta horários ao trocar a data do date input
+
 dateInputMain.addEventListener("change", () => {
     selectedDate = dateInputMain.value;
     renderSchedules(selectedDate);
@@ -370,12 +326,8 @@ window.addEventListener("DOMContentLoaded", () => {
     renderSchedules(dateInputMain.value);
 })
 
-////////////////////////////////////////////////
-//  Todas as funções serão chamadas abaixo    //
-////////////////////////////////////////////////
 
-// ---registerSchedule()--- ESTÁ EM form.addEventListenear !!
 todayDate();
 dialogDate(today);
-hoursLoad(dateInput.value); // Load hours initially based on the default date.
+hoursLoad(dateInput.value); 
 unavailableHours();
